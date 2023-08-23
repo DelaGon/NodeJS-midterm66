7d8f192a8ca17ea528ce72356d26e38801f22f05
DelaGon <87527854+DelaGon@users.noreply.github.com>
2023-08-23 12:44:19 +0700

    1-2

---
+++ src/controllers/mid66.js
@@ -0,0 +1,48 @@
+module.exports = {
+add(req, res) {
+input = parseInt(JSON.stringify(req.body["input"]));
+console.log("input = " + input);
+output = input + 1;
+res.send(output.toString());
+    },
+
+//ข้อที่1 X--
+func1(req, res) {
+    n = parseInt(JSON.stringify(req.body["input"]));
+    console.log("input = " + n);
+//let x = 3;
+//const y = x--;
+//console.log(`x:${x}, y:${y}`);
+// Expected output: "x:2, y:3"
+output = n--;
+console.log('output: ' + output);
+res.send(output.toString());
+},
+
+//ข้อ2 x!
+func2(req, res) {
+// program to find the factorial of a number
+// take input from the user
+//const number = parseInt(prompt('Enter a positive integer: '));
+// checking if number is negative
+number = parseInt(JSON.stringify(req.body["input"]));
+console.log("Enter a positive integer: " + number);
+
+if (number < 0) {
+    console.log('Error! Factorial for negative number does not exist.');
+}
+// if number is 0
+else if (number === 0) {
+    console.log(`The factorial of ${number} is 1.`);
+}
+// if number is positive
+else {
+    let fact = 1;
+    for (i = 1; i <= number; i++) {
+     fact *= i;
+}
+    console.log(`The factorial of ${number} is ${fact}.`);
+}
+}
+};
+    
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,21 @@
-const UserController =require('./controllers/UserController')
+const Midterm = require('./controllers/mid66');
+const UserController = require('./controllers/UserController')
 
 module.exports = (app) => {
-    app.post('/users',UserController.create)
-    app.put('/user/:userID',UserController.put)
-    app.delete('/user/:userID',UserController.remove)
-    app.get('/user/:userID',UserController.show)
-    app.get('/user',UserController.index)
+    app.post('/user', UserController.create)
+    
+    app.put('/user', UserController.put)
+    
+    app.delete('/user', UserController.remove)
+    
+    app.get('/user', UserController.show)
+    
+    app.get('/users', UserController.index)
+
+    app.post('/add', Midterm.add)
+
+    app.post('/func1', Midterm.func1)
+
+    app.post('/func2', Midterm.func2)
+
 }
